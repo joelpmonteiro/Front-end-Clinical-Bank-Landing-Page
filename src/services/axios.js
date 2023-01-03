256bc1023fe3c526f067b8ec77b8702d510465f9
joel de paula monteiro <37256731+joelpmonteiro@users.noreply.github.com>
2023-01-03 09:17:28 -0300

    update file axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+import * as jose from "jose";
+import md5 from "md5";
+
+const company_id = "b00cd1fd-b20f-49f1-97a2-88407d36c7fd";
+const user_id = "31b86d3e-9790-4b88-9846-c337f16f385c";
 
 const api = axios.create({
   baseURL: "http://177.136.201.182/api/",
+  //baseURL: "http://localhost:3333/api/",
 });
 
 const api02 = axios.create({
@@ -11,21 +17,79 @@ const api02 = axios.create({
   },
 });
 
+async function auth(data) {
+  const secret = new TextEncoder().encode(
+    md5(
+      `${JSON.stringify({
+        company_id,
+        user_id,
+        ...data,
+      })}:${user_id}:${company_id}`
+    )
+  );
+
+  const token = await new jose.SignJWT({
+    company_id,
+    user_id,
+    ...data,
+  })
+    .setProtectedHeader({ alg: "HS256" })
+    .sign(secret);
+
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+}
+
 export const postDataNeurotech = async (data) => {
-  const neurotech = await api.post("debt/consult", data);
+  const neurotech = await api.post(
+    "debt/consult?simple",
+    {
+      company_id,
+      user_id,
+      ...data,
+    },
+    await auth(data)
+  );
   return neurotech;
 };
 
 export const creditSimulation = async (data) => {
-  return await api.post("debt/simulation", data);
+  return await api.post(
+    "debt/simulation?simple",
+    {
+      company_id,
+      user_id,
+      ...data,
+    },
+    await auth(data)
+  );
 };
 
 export const requestDataUser = async (data) => {
-  return await api.post("customer/create", data);
+  return await api.post(
+    "customer/create?simple",
+    {
+      company_id,
+      user_id,
+      ...data,
+    },
+    await auth(data)
+  );
 };
 
 export const makePaymentCardCredit = async (data) => {
-  const response = api.post("billing/create", data);
+  const response = api.post(
+    "billing/create?simple",
+    {
+      company_id,
+      user_id,
+      ...data,
+    },
+    await auth(data)
+  );
   return response;
 };
 
